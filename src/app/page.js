8bc5cff23adbf187d44cd60c300b1c407ce94213
Fatima Tabassum <tabassumfatima309@gmail.com>
2024-03-04 15:54:32 +0530

    added Review page and working on order summary

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,16 @@ import Header from "@/components/buyer/home/Header";
 import MyBasket from "@/components/buyer/home/MyBasket";
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
     <div className="container-fluid flex flex-col justify-center items-center  w-[100%] min-h-[100vh] gap-[5vh]">
       <Header />
       <MyBasket />
       <Footer />
+      
+      {/* <OrderSummary/> */}
     </div>
   );
 }
