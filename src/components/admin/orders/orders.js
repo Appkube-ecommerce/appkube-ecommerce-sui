ced5870baa4c9f227cdf943c3bf8928f8a8e7454
shaistha-samreen <156286615+shaistha-samreen@users.noreply.github.com>
2024-03-08 09:58:19 +0530

    Update orders.js

--- src/components/admin/orders/orders.js
+++ src/components/admin/orders/orders.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 //import React from 'react';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
@@ -204,14 +203,4 @@ const Orders = () => {
 };
 
 export default Orders;
-=======
-import React from 'react'
 
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
-
-export default Orders
->>>>>>> 625d693eff04d2cc978fc86177156f6234703762
