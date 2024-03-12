bda8bdf85d4378d672b69ddf7a5497c06cc9f4a2
Fatima Tabassum <tabassumfatima309@gmail.com>
2024-03-12 10:38:48 +0530

    Printing Products.using Api for fetching products.added this print product button in product page

--- src/components/admin/products/products.js
+++ src/components/admin/products/products.js
@@ -7,6 +7,7 @@ import ImportButton from "./importButton";
 import { useRouter } from "next/navigation";
 import Link from "next/link"
 import { Radio } from 'antd';
+import ProductList from "../productsList/Pro";
 // import Addproduct from "./addproduct";
 
 const data = [
@@ -250,7 +251,9 @@ const Products = () => {
     <>
       <header className="flex justify-between mt-4">
         <h1 className="font-bold text-2xl">Products</h1>
+      
         <div className="flex gap-3">
+         
        
           <button
             style={{
@@ -354,6 +357,8 @@ const Products = () => {
         pagination={false}
         scroll={{ x: 1000, y: 900 }}
       />
+      {/* calling print button in product page */}
+        <ProductList/>
     </>
   );
 };
