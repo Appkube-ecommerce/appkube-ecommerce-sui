59480cb470e663bd58a2ad8ebc1c794ec7de018d
MAIMUNA-GULAFSHAN <maimunagulafshan1@gmail.com>
2024-04-03 05:01:38 +0530

    worked on myyAccount page

--- src/app/buyer/myAccount/page.js
+++ src/app/buyer/myAccount/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MyAccountPage from '@/components/buyer/myAccont/myAccount'
 const Page = () => {
   return (
-    <div>MyAccount</div>
+    <div><MyAccountPage/></div>
   )
 }
 
