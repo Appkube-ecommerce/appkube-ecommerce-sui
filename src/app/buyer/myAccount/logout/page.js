3fa26bc125d0b0ff0900224ff3c58eecdffb090a
MAIMUNA-GULAFSHAN <maimunagulafshan1@gmail.com>
2024-04-02 12:14:14 +0530

    my account page to navigate over logout, MyAccount, MyOrders

---
+++ src/app/buyer/myAccount/logout/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Logout from '@/components/buyer/myAccont/logout/logout'
+const Page = () => {
+  return (
+    <div><Logout/></div>
+  )
+}
+
+export default Page
---
+++ src/components/buyer/myAccont/myAccount.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyAccountPage = () => {
+  return (
+    <div>gfghjk876567890hbcbdcdcdcdcc</div>
+  )
+}
+
+export default MyAccountPage
